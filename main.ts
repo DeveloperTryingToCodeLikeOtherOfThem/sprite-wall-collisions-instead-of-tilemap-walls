b1833c5cb758b47593d21f10609c53ed40f69819
a012 <Aeagan0507@gmail.com>
2025-10-04 13:36:48 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -6,8 +6,9 @@ namespace spriteWalls {
         Off
     }
 
-   //% block="set sprite wall collision %sprite=variables_get(playerSprite) %wallSpriteObstacle=variables_get(wallSpriteObstacle) %on=SpriteWallCollisions.On"
-    export function setSpriteWallCollision(sprite: Sprite, wallSpriteObstacle: Sprite, on: SpriteWallCollisions | boolean) {
+   //% block="set sprite wall collision %sprite=variables_get(playerSprite) %wallSpriteObstacle=variables_get(wallSpriteObstacle) %on"
+   //% on.fieldEditor="gridpicker"
+   export function setSpriteWallCollision(sprite: Sprite, wallSpriteObstacle: Sprite, on: SpriteWallCollisions | boolean) {
        if(on === true || on === SpriteWallCollisions.On)
         if(sprite.overlapsWith(wallSpriteObstacle)) {
             if (sprite.x <= wallSpriteObstacle.x 
